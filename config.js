4af1057674caa0f1889cc68280d49cadab48dfaf
a-kuei-cy <tea@sces.cy.edu.tw>
2026-09-17 20:09:33 +0800

    更新 config.js via EZPage

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SCC_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzVMLaFZ3FgcGvTT5eSDQ440W3H0EhQWmzkZ6uwBc_FlA_aXfnoevp4CNFhSqjMfrIe/exec",
+  APP_NAME: "Sing Chia Class",
+  VERSION: "V1.1 School"
+};
